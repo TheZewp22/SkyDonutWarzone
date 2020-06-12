7b4fe0498d8c4febaac01eea87c9dbc87c840133
SdotMousse <66333568+SdotMousse@users.noreply.github.com>
2020-06-12 17:58:03 +0100

    Newproject
    New project

---
+++ Sky_Warzone_Donuts/Assets/Scripts/TheZewp22's Scripts/gameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+//  The Current Level The Player is In
+public static int currentLevel;
+
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    currentLevel = SceneManager.GetActiveScene().buildIndex;
+  }
+
+
+
+
+
+
+
+
+
+
+}
